f8de7708f8a221482db41617555d1b3bb87942ab
yuika nagata <xxx.yu.74@gmail.com>
2022-10-29 20:10:55 +0900

    第７回課題　修正

--- src/main/java/com/example/task7/CreateForm.java
+++ src/main/java/com/example/task7/CreateForm.java
@@ -4,19 +4,31 @@ import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 public class CreateForm {
     @NotBlank
     @Length(max = 20)
     private String name;
+    @NotBlank
+    @Pattern(regexp = "^[0-9]{4}/(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])$")
+    private String birthday;
 
     public String getName() {
         return name;
 
     }
 
+    public String getBirthday() {
+        return birthday;
+    }
+
     public void setName(String name) {
 
         this.name = name;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
     }
 }
--- src/main/java/com/example/task7/Task7Controller.java
+++ src/main/java/com/example/task7/Task7Controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import java.net.URI;
 import java.util.List;
@@ -16,18 +17,18 @@ import java.util.Map;
 public class Task7Controller {
 
     @GetMapping("/name")
-    public ResponseEntity<Map<String, String>> getUser(@RequestParam(value = "name", defaultValue = "name") String name, @RequestParam @Pattern(regexp = "(0[1-9]|1[0-2])(0[1-9]|1\\d|2\\d|3[0-1])$") String birthday) {
+    public ResponseEntity<Map<String, String>> getUser(@RequestParam(value = "name") String name, @RequestParam @Pattern(regexp = "(0[1-9]|1[0-2])(0[1-9]|1\\d|2\\d|3[0-1])$") String birthday) {
 
-        return ResponseEntity.ok(Map.of("message", name + " " + birthday));
+        return ResponseEntity.ok(Map.of("name", name, "birthday", birthday));
     }
 
     @PostMapping("/names")
-    public ResponseEntity<String> create(@RequestBody @Validated CreateForm form) {
+    public ResponseEntity<Map<String, String>> create(@RequestBody @Validated CreateForm form) {
         URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
                 .path("/name/id")
                 .build()
                 .toUri();
-        return ResponseEntity.created(url).body("name successfully created");
+        return ResponseEntity.created(url).body(Map.of("message", "name successfully created"));
     }
 
     @PatchMapping("/names/{id}")
